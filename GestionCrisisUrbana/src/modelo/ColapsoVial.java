d7d7ed172c77867626d749e7e51ace5a0cb9d5ec
lagui <lagui@LuisAguirre-69>
2026-02-01 12:37:29 -0500

    Colapso Vial casi

---
+++ GestionCrisisUrbana/src/modelo/ColapsoVial.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.util.Random;
+
+/**
+ *
+ * @author lagui
+ */
+public class ColapsoVial {
+    private static Random c = new Random();
+    
+    // ===== FALLOS PRINCIPALES =====
+    private static String[][] fallosC = {
+        {"Disturbios masivos", "CRÍTICO", "Perdoda de control de las personas"},
+        {"Robo organizado", "MEDIO", "Miedo en la población"},
+        {"Incidente aislado", "BAJO", "Ingresos delictivos de gente"}      
+    };
+}
