92395670c6740d5deac6ae00c0d12a26f1973a04
lagui <lagui@LuisAguirre-69>
2026-02-01 13:15:04 -0500

    Colapso Vial

--- GestionCrisisUrbana/src/modelo/ColapsoVial.java
+++ GestionCrisisUrbana/src/modelo/ColapsoVial.java
@@ -15,8 +15,36 @@ public class ColapsoVial {
     
     // ===== FALLOS PRINCIPALES =====
     private static String[][] fallosC = {
-        {"Disturbios masivos", "CRÍTICO", "Perdoda de control de las personas"},
-        {"Robo organizado", "MEDIO", "Miedo en la población"},
-        {"Incidente aislado", "BAJO", "Ingresos delictivos de gente"}      
+        {"Colapso vial", "CRÍTICO", "Congestión masiva"},
+        {"Semáforos Dañados", "MEDIO", "Cierre de vías"},
+        {"Congestión leve", "BAJO", "Tráfico concurrente"}      
     };
+    
+    // ===== cascadas =====
+    private static String[][] cascadasC = {
+          {"Robo a mano armada", "CRÍTICO", "Gente en la calle"},
+          {"Múltiples accidentes", "CRÍTICO", "Personas heridas"},
+          {"Sistema de semáforos dañado", "MEDIO", "Inmobilidad de carros"},
+          {"Malestar social", "MEDIO", "Accidentes concurrentes"}
+    };
+    
+    public static Evento generarFalloC(int id){
+        int i = c.nextInt(fallosC.length);
+        return new Evento(
+        id,
+        fallosC[i][0],
+        fallosC[i][1],
+        fallosC[i][2]
+        );
+    }
+    
+    public static Evento generarCascadaP(int id){
+        int i = c.nextInt(cascadasC.length);
+        return new Evento(
+        id,
+        fallosC[i][0],
+        fallosC[i][1],
+        "Problema derivado del colapso vial"
+        );
+    }
 }
