d3f08a013befbbc8cc8fe7961d8013502ae4afe5
lagui <lagui@LuisAguirre-69>
2026-02-03 22:30:25 -0500

    Comentar codigos y botones

--- GestionCrisisUrbana/src/Nodos/NodoEvento.java
+++ GestionCrisisUrbana/src/Nodos/NodoEvento.java
@@ -12,12 +12,12 @@ import modelo.Evento;
  */
 
 public class NodoEvento {
-    public Evento info;
-    public NodoEvento liga;
+    public Evento info;// Atributo que almacena la información del evento y cada nodo contiene un objeto de tipo Evento
+    public NodoEvento liga; // Permite enlazar este nodo con otro formando una lista enlazada
 
-    public NodoEvento(Evento e) {
-        this.info = e;
-        this.liga = null;
+    public NodoEvento(Evento e) { // Recibe un objeto Evento y lo asigna al nodo
+        this.info = e; // Se guarda el evento dentro del nodo
+        this.liga = null; // Inicialmente el nodo no apunta a ningún otro nodo
     }
 }
 
