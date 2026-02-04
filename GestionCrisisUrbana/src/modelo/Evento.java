158319152757c6abf4b97c3f5a8f1676ff999075
lagui <lagui@LuisAguirre-69>
2026-02-04 10:24:50 -0500

    Comentar codigos y botones

--- GestionCrisisUrbana/src/modelo/Evento.java
+++ GestionCrisisUrbana/src/modelo/Evento.java
@@ -11,11 +11,12 @@ import java.time.LocalTime;
  * @author ASUS
  */
 public class Evento {
-    private int id;
-    private String tipo;        // Eléctrico, Agua, Salud, etc
-    private String nivel;          // 3 = crítico, 2 = medio, 1 = bajo
-    private String descripcion;
+    private int id; //Identificador unico del evento
+    private String tipo;        // Tipo de evento (eléctrico, agia, salu, etc.)
+    private String nivel;          // Nivel de gravedad (CRÍTICO, MEDIO, BAJO)
+    private String descripcion; //Descripción del evento
 
+    //Permite crear un nuevo evento asignando sus atributos principales
     public Evento(int id, String tipo, String nivel, String descripcion) {
         this.id = id;
         this.tipo = tipo;
@@ -24,19 +25,19 @@ public class Evento {
     }
     
     
-    public int getId() {
+    public int getId() { //Método que retorna el identificador del evento
         return id;
     }
 
-    public String getTipo() {
+    public String getTipo() { //Método que retorna el tipo de evento
         return tipo;
     }
 
-    public String getNivel() {
+    public String getNivel() { //Método que retorna el nivel de gravedad del evento
         return nivel;
     }
 
-    public String getDescripcion() {
+    public String getDescripcion() { //Método que retorna la descripción del evento
         return descripcion;
     }
 
